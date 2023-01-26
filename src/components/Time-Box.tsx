04dfce9ea4f4bd0297d81ab17b1a16428b1759ed
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-01-26 14:49:09 +0900

    modify CSS

--- src/components/Time-Box.tsx
+++ src/components/Time-Box.tsx
@@ -4,25 +4,27 @@ export const TimeList = () => {
     .map((time, i) => (time + i).toString().padStart(2, "0"));
 
   return (
-    <ol className="bg-rose-200 px-4">
+    <ol className="px-4 h-full">
       {timeList.map((time) => (
-        <li key={time}>{time}</li>
+        <li className="pb-10 text-xs h-12 " key={time}>
+          {time}
+        </li>
       ))}
     </ol>
   );
 };
 
 export const TimeContainer = () => {
-  return <div className="border-b-1">to-do</div>;
+  return <div className="h-12 border-b-2 border-blue-200">to-do</div>;
 };
 
 export default function TimeBox() {
-  let mockArr = new Array(25).fill("...");
+  let mockArr = new Array(24).fill("...");
 
   return (
-    <div className="flex w-full">
+    <div className="flex h-200 w-96 overflow-auto shadow-lg">
       <TimeList />
-      <div className="grow bg-gray-200">
+      <div className="grow pt-2">
         {mockArr.map((todo, i) => (
           <TimeContainer key={i} />
         ))}
