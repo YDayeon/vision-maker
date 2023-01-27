aa828ab3eaecd9fd4043a5fbcc420881fefc8860
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-01-27 17:56:58 +0900

    config css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,31 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      width: {
+        152: "38rem",
+        168: "42rem",
+      },
       height: {
+        76: "19rem",
+        152: "38rem",
+        168: "42rem",
         200: "50rem",
+      },
+      gridTemplateColumns: {
+        "1rem": "1rem 1fr 1rem",
+      },
+      gridTemplateRows: {
+        "1rem": "1rem 1fr 1rem",
       },
       transitionProperty: {
         width: "width",
+      },
+      inset: {
+        36: "9rem",
+      },
+      boxShadow: {
+        "direct-l": "4px 4px gray",
+        "direct-xl": "8px 8px gray",
       },
     },
   },
