9623ddd041c1d9a53ae7f7a051fb27d2fd89a1e7
Dayeon Yoon <yday1223@gmail.com>
2023-01-29 14:30:42 +0900

    init setting and add Timer page to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { CgMenu } from "react-icons/cg";
-import { HiOutlineHome } from "react-icons/hi";
-import { TbCalendar } from "react-icons/tb";
-import { MdOutlineFactCheck } from "react-icons/md";
-import Link from "next/link";
+import { useState } from 'react';
+import { CgMenu } from 'react-icons/cg';
+import { HiOutlineHome } from 'react-icons/hi';
+import { TbCalendar } from 'react-icons/tb';
+import { MdOutlineFactCheck } from 'react-icons/md';
+import { RxLapTimer } from 'react-icons/rx';
+import Link from 'next/link';
 
 export default function NavBar() {
   const [isFold, setIsFold] = useState(false);
@@ -15,40 +16,49 @@ export default function NavBar() {
   return (
     <nav
       className={`bg-slate-300 h-full px-5 transition-all duration-500 ease-in-out w-52 ${
-        isFold && "bg-slate-600 w-fit"
+        isFold && 'bg-slate-600 w-fit'
       }`}
     >
       <CgMenu
-        color={isFold ? "whitesmoke" : ""}
-        className="w-7 h-7 mt-4 mb-8"
+        color={isFold ? 'whitesmoke' : ''}
+        className='w-7 h-7 mt-4 mb-8'
         onClick={() => setIsFold(!isFold)}
       />
-      <div className="flex flex-col gap-5">
+      <div className='flex flex-col gap-5'>
         <div>
-          <Link href={"/"} className="flex gap-2">
+          <Link href={'/'} className='flex gap-2'>
             <HiOutlineHome
-              color={isFold ? "whitesmoke" : ""}
-              className="w-6 h-6"
+              color={isFold ? 'whitesmoke' : ''}
+              className='w-6 h-6'
             />
-            {!isFold && "Home"}
+            {!isFold && 'Home'}
           </Link>
         </div>
         <div>
-          <Link href={"/calendar"} className="flex gap-2">
+          <Link href={'/timer'} className='flex gap-2'>
+            <RxLapTimer
+              color={isFold ? 'whitesmoke' : ''}
+              className='w-6 h-6'
+            />
+            {!isFold && 'Timer'}
+          </Link>
+        </div>
+        <div>
+          <Link href={'/calendar'} className='flex gap-2'>
             <TbCalendar
-              color={isFold ? "whitesmoke" : ""}
-              className="w-6 h-6"
+              color={isFold ? 'whitesmoke' : ''}
+              className='w-6 h-6'
             />
-            {!isFold && "Calendar"}
+            {!isFold && 'Calendar'}
           </Link>
         </div>
         <div>
-          <Link href={"/review"} className="flex gap-2">
+          <Link href={'/review'} className='flex gap-2'>
             <MdOutlineFactCheck
-              color={isFold ? "whitesmoke" : ""}
-              className="w-6 h-6"
+              color={isFold ? 'whitesmoke' : ''}
+              className='w-6 h-6'
             />
-            {!isFold && "Review"}
+            {!isFold && 'Review'}
           </Link>
         </div>
       </div>
