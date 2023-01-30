b0a5d838854f7db4cb33b994c8632001008cd34f
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-01-30 17:59:16 +0900

    setting change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
     extend: {
       width: {
-        152: '38rem',
-        168: '42rem',
+        152: "38rem",
+        168: "42rem",
+        "1/7": "14.2857%",
       },
       height: {
-        76: '19rem',
-        152: '38rem',
-        168: '42rem',
-        200: '50rem',
-        '9/10': '90%',
+        76: "19rem",
+        152: "38rem",
+        168: "42rem",
+        200: "50rem",
+        "9/10": "90%",
       },
       gridTemplateColumns: {
-        '1rem': '1rem 1fr 1rem',
+        "1rem": "1rem 1fr 1rem",
       },
       gridTemplateRows: {
-        '1rem': '1rem 1fr 1rem',
+        "1rem": "1rem 1fr 1rem",
       },
       transitionProperty: {
-        width: 'width',
+        width: "width",
       },
       inset: {
-        36: '9rem',
+        36: "9rem",
       },
       boxShadow: {
-        'direct-l': '4px 4px gray',
-        'direct-xl': '8px 8px gray',
+        "direct-l": "4px 4px gray",
+        "direct-xl": "8px 8px gray",
       },
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide'),
-    require('tailwindcss')('./src/tailwind.config.js'),
-    require('autoprefixer'),
+    require("tailwind-scrollbar-hide"),
+    require("tailwindcss")("./src/tailwind.config.js"),
+    require("autoprefixer"),
   ],
 };
