8206b4ec4a47a75710531b88240e2d8801a4a2c8
Dayeon Yoon <yday1223@gmail.com>
2023-02-02 23:48:54 +0900

    modify setting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig;
+// config.resolve.modules.push(__dirname);
